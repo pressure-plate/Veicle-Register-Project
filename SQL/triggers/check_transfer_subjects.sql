e2489fc65e3209ad17bd5ff37564f9d0c79a7960
SimoneABNto <smn.not@outlook.com>
2020-08-20 20:17:00 +0200

    Trigger controlla coggetti cessione

---
+++ SQL/triggers/check_transfer_subjects.sql
@@ -0,0 +1,28 @@
+create or replace function controlla_soggetti_cessione()
+returns trigger
+language plpgsql as
+$$
+    declare
+        vecchio_propietario id_propietario;
+	    nuovo_propietario id_propietario;
+    
+    begin
+        SELECT cf INTO vecchio_propietario FROM Propietario
+            WHERE cf = new.vecchio_propietario;
+        
+        SELECT cf INTO nuovo_propietario FROM Propietario
+            WHERE cf = new.nuovo_propietario;
+
+        -- se la persona e' la stessa abort
+        if (vecchio_propietario == nuovo_propietario) then
+            raise exception 'In proprietario non puo cedere il veicolo a se stesso';
+        end if;
+        return new;
+    end;
+$$;
+
+
+create trigger controlla_soggetti_cessione
+before insert on Cessione
+for each row
+execute procedure controlla_soggetti_cessione();
