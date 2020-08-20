b9d68db95e09700fdc824a243e16df7f8ac69c9c
SimoneABNto <smn.not@outlook.com>
2020-08-20 19:44:13 +0200

    create schema fix

--- SQL/create_schema.sql
+++ SQL/create_schema.sql
@@ -1,44 +1,68 @@
 -- CREATE SCHEMA VehicleRegister;
 
 
+DROP TABLE IF EXISTS Versione;
 DROP TABLE IF EXISTS Cessione;
-DROP TABLE IF EXISTS Propietario;
 DROP TABLE IF EXISTS VeicoloImmatricolato;
 DROP TABLE IF EXISTS Modello;
 DROP TABLE IF EXISTS CasaProduttrice;
-DROP TABLE IF EXISTS Versione;
+DROP TABLE IF EXISTS Propietario;
 
 DROP SEQUENCE IF EXISTS id_pratica_serial;
 DROP SEQUENCE IF EXISTS id_versione_serial;
 
+DROP TYPE IF EXISTS alimentazioni;
+DROP TYPE IF EXISTS veicoli;
+
+DROP DOMAIN IF EXISTS id_propietario;
+DROP DOMAIN IF EXISTS id_veicolo_immatricolato;
+
+-- creazione delle 2 sequenza auto incrementanti
 CREATE SEQUENCE id_pratica_serial;
 CREATE SEQUENCE id_versione_serial;
 
+-- creazione di un tipo per l'enumerazione
+CREATE TYPE alimentazioni AS ENUM('diesel', 'benzina', 'metano', 'gpl', 'elettrico', 'altro');
+CREATE TYPE veicoli AS ENUM('macchina', 'moto', 'motociclo', 'camion', 'trattore', 'altro');
+
+-- creazione di tipi dedicati per le chiavi primarie che devono essere usate come fk
+CREATE DOMAIN string AS varchar(60);
+CREATE DOMAIN id_propietario AS varchar(20);
+CREATE DOMAIN id_veicolo_immatricolato AS varchar(10);
+
+
 CREATE TABLE Propietario(
-  cf varchar(20),
-  nome varchar(20),
-  cognome varchar(20),
-  residenza varchar(50),
+  cf id_propietario,
+  nome string,
+  cognome string,
+  residenza string,
   PRIMARY KEY(cf)
 );
 
 CREATE TABLE CasaProduttrice
 (
-  nome varchar(20),
-  direttore varchar(30),
-  contatto_telefonico varchar(30),
-  indirizzo_sede varchar(30),
-  gruppo varchar(30),
+  nome string,
+  direttore string,
+  contatto_telefonico string,
+  indirizzo_sede string,
   PRIMARY KEY(nome)
 );
 
 create table Modello
 (
   id_modello integer,
-  nome_modello varchar(10),
-  alimentazione int,
-  casa_produttrice varchar(20), -- fk
+  cilindrata real,
+  cavalli_fiscali real,
+  velocita_massima int,
+  numero_posti int,
+  alimentazione alimentazioni,
+  classe_veicolo veicoli, -- tipo di veicolo auto, moto, camion
+  motorizzazione string, -- dettagli sul tipo di motore
+  
+  casa_produttrice string, -- fk
+  
   PRIMARY KEY(id_modello),
+  
   CONSTRAINT fk_casa_produttrice
     FOREIGN KEY(casa_produttrice)
       REFERENCES CasaProduttrice(nome)
@@ -46,14 +70,18 @@ create table Modello
 
 CREATE TABLE VeicoloImmatricolato
 (
-	targa varchar(10) not null,
-	data_ummatricolazione timestamp not null,
-  propietario varchar(20), -- fk
+	targa id_veicolo_immatricolato not null,
+	data_immatricolazione timestamp not null,
+  
+  propietario id_propietario, -- fk
   modello integer, -- fk
+  
   PRIMARY KEY(targa),
+  
   CONSTRAINT fk_propietario
     FOREIGN KEY(propietario)
       REFERENCES Propietario(cf),
+  
   CONSTRAINT fk_modello
     FOREIGN KEY(modello)
       REFERENCES Modello(id_modello)
@@ -62,11 +90,22 @@ CREATE TABLE VeicoloImmatricolato
 CREATE TABLE Cessione
 (
 	id_pratica integer NOT NULL DEFAULT nextval('id_pratica_serial'),
-	ex_propietario varchar(10),
-	nuovo_propietario varchar(10),
-	data_passaggio timestamp,
-  veicolo_immatricolato varchar(10), -- fk
-	PRIMARY KEY(id_pratica),
+  data_passaggio timestamp,
+	
+  vecchio_propietario id_propietario, -- fk
+	nuovo_propietario id_propietario, -- fk
+  veicolo_immatricolato id_veicolo_immatricolato, -- fk
+	
+  PRIMARY KEY(id_pratica),
+
+  CONSTRAINT fk_vecchio_propietario
+		FOREIGN KEY(vecchio_propietario)
+      REFERENCES Propietario(cf),
+
+  CONSTRAINT fk_nuovo_propietario
+		FOREIGN KEY(nuovo_propietario)
+      REFERENCES Propietario(cf),
+
 	CONSTRAINT fk_veicolo_immatricolato
 		FOREIGN KEY(veicolo_immatricolato)
 		  REFERENCES VeicoloImmatricolato(targa)
@@ -74,10 +113,16 @@ CREATE TABLE Cessione
 
 create table Versione
 (
-  id_versione integer NOT NULL DEFAULT nextval('id_versione_serial'),
   numero_versione int,
   numero_pezzi_prodotti int,
   data_inizio_produzione date,
   data_fine_produzione date,
-  PRIMARY KEY(id_versione)
+
+  modello integer, --fk
+
+  PRIMARY KEY(numero_versione, modello),
+
+  CONSTRAINT fk_modello
+		FOREIGN KEY(modello)
+		  REFERENCES Modello(id_modello)
 );
