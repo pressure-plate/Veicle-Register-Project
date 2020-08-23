5c1c4597be3a06a6a97f3dc4fd91d0fa42ed7ce3
Maestro <andreamaestro98@gmail.om>
2020-08-23 09:35:24 +0200

    add triggers for veicolo_immatricolato

--- SQL/triggers/veicolo_immatricolato.sql
+++ SQL/triggers/veicolo_immatricolato.sql
@@ -1,1 +1,17 @@
 -- un veicolo immatricolato non puo' essere mai eliminato
+create or replace function controlla_veicolo_immatricolato()
+returns trigger
+language plpgsql as
+$$
+    begin
+        raise notice 'You cannot delete a record in this table'
+        return null;
+    end;
+$$;
+
+create trigger controlla_veicolo_immatricolato
+before delete on veicoloimmatricolato
+for each row
+execute procedure controlla_veicolo_immatricolato(); 
+
+-- La Versione del veicolo immatricolato deve essere appartenere al Modello immatricolato
