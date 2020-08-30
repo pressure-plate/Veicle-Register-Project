42be78b593acc716ab9e6bc15fa5361d878d0a6d
albe96 <albertonadin@ymail.com>
2020-08-30 16:39:30 +0200

    Aggiunte commenti e sistemazione file R

---
+++ PROGETTO/analizza.r
@@ -0,0 +1,50 @@
+#Carico in memoria il driver per PostgreSQL e mi connetto al database.
+
+install.packages("RPostgreSQL")
+library("RPostgreSQL")
+drv = dbDriver("PostgreSQL")
+con = dbConnect(drv ,
+                dbname = "postgres", 
+                host =  "127.0.0.1", 
+                port = "5432", 
+                user = "postgres", 
+                password = "admin")
+
+print(con)
+
+# Recupero i dati sulle auto più vendute interrogando la base di dati
+auto = dbGetQuery(con,
+                   "set search_path to MotorizzazioneCivile, public;
+                   
+                    select modello, count(*) as n
+                    from veicoloimmatricolato
+                    group by modello;" )
+
+# Grafico delle case automobilistiche con più immatricolazioni
+k=6
+barplot(height = auto$n[1:k],
+        names.arg = auto$modello[1:k],
+        main ="Modelli più immatricolati",
+        ylab ="Numero immatricolazioni",
+        xlab ="Modello",
+        cex.names =0.9)
+
+
+# Recupero i dati sugli allestimenti interrogando la base di dati
+allestimentiAuto = dbGetQuery(con,
+                   "set search_path to MotorizzazioneCivile, public;
+                   
+                    select nome, count(*) as x
+                    from allestimento
+                    group by nome;" )
+# Grafico degli allestimenti più scelti.
+k=5
+barplot(height = allestimentiAuto$x[1:k],
+        names.arg = allestimentiAuto$nome[1:k],
+        main ="Allestimenti più Scelti",
+        ylab ="Percentuale scelta dell'allestimento ",
+        xlab ="Allestimento",
+        cex.names =0.9)      
+                     
+# Chiudo la connessione con il server.        
+dbDisconnect(con)
---
+++ PROGETTO/popola.r
@@ -0,0 +1,44 @@
+# Questo script popola la relazione ‘CasaProduttrice’ con tuple generate casualmente
+
+#Carico in memoria il driver per PostgreSQL e mi connetto al database.
+
+install.packages("RPostgreSQL")
+library("RPostgreSQL")
+drv = dbDriver("PostgreSQL")
+con = dbConnect(drv ,
+                dbname = "postgres", 
+                host =  "127.0.0.1", 
+                port = "5432", 
+                user = "postgres", 
+                password = "admin")
+
+print(con)
+
+# Creo i data frame con cui popolerò il database recuperando i dati dai file csv
+
+proprietario = read.csv("./PROGETTO/csv/proprietario.csv")
+casaProduttrice = read.csv("./PROGETTO/csv/casaProduttrice.csv")
+modello = read.csv("./PROGETTO/csv/modello.csv")
+allestimento = read.csv("./PROGETTO/csv/allestimento.csv")
+veicoloImmatricolato = read.csv("./PROGETTO/csv/veicoloImmatricolato.csv")
+cessione = read.csv("./PROGETTO/csv/cessione.csv")
+
+# Inserisco le tuple nelle varie relazioni           
+
+    dbWriteTable(con, name=c("motorizzazionecivile", "propietario"), value=proprietario, append=T, row.names=F)
+
+    dbWriteTable( con, name=c("motorizzazionecivile", "casaproduttrice"), value=casaProduttrice, append=T, row.names=F)
+
+    dbWriteTable( con, name=c("motorizzazionecivile", "modello"), value=modello, append=T, row.names=F)
+
+    dbWriteTable( con, name=c("motorizzazionecivile", "allestimento"), value=allestimento, append=T, row.names=F)
+
+    dbWriteTable( con, name=c("motorizzazionecivile", "veicoloimmatricolato"), value=veicoloImmatricolato, append=T, row.names=F)
+
+    dbWriteTable( con, name=c("motorizzazionecivile", "cessione"), value=cessione, append=T, row.names=F)
+
+
+
+# Chiudo la connessione con il server
+
+dbDisconnect(con)
